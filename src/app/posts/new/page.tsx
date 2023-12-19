6cab81e05e62f68340868b8ac46fd2c2a31a7baf
Seth Randall <sethjrandall@gmail.com>
2023-12-18 23:08:59 -0500

    add styles for new post page

--- src/app/posts/new/page.tsx
+++ src/app/posts/new/page.tsx
@@ -2,9 +2,11 @@ import { CreatePost } from "~/app/_components/create-post";
 
 export default async function Page() {
   return (
-    <div>
-      New Page
-      <CreatePost />
+    <div className="min-h-screen m-auto bg-gradient-to-b from-[#2e026d] to-[#15162c] flex justify-center">
+      <div className="lg:w-4/12 w-9/12">
+        <h1 className="text-center text-5xl m-3">New Post</h1>
+        <CreatePost />
+      </div>
     </div>
   );
 }
